f15a6812eb9f28afc5c9657d18006c4f8642ea62
hjoo830 <hjoo830@naver.com>
2023-08-04 17:53:40 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Board from "./Board";
+import Button from './Button';
+import { useState } from 'react';
+
+function random(n) {
+    return Math.ceil(Math.random() * n);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [num, setNum] = useState(1);
+    const [sum, setSum] = useState(0);
+    const [gameHistory, setGameHistory] = useState([]);
+    const [otherNum, setOtherNum] = useState(1);
+    const [otherSum, setOtherSum] = useState(0);
+    const [otherGameHistory, setOtherGameHistory] = useState([]);
+
+    const handleRollClick = () => {
+        const nextNum = random(6);
+        const nextOtherNum = random(6);
+        
+        setNum(nextNum);
+        setSum(sum+nextNum);
+        setGameHistory([...gameHistory, nextNum]);
+
+        setOtherNum(nextOtherNum);
+        setOtherSum(otherSum+nextOtherNum);
+        setOtherGameHistory([...otherGameHistory, nextOtherNum]);
+    };
+    
+    const handleClearClick = () => {
+        setNum(1);
+        setSum(0);
+        setGameHistory([]);
+        setOtherNum(1);
+        setOtherSum(0);
+        setOtherGameHistory([]);
+    };
+
+    return (
+        <div>
+            <Button onClick={handleRollClick}>던지기</Button>
+            <Button onClick={handleClearClick}>처음부터</Button>
+            <div>
+                <Board name="나" color="blue" num={num} sum={sum} gameHistory={gameHistory} />
+                <Board name="상대" color="red" num={otherNum} sum={otherSum} gameHistory={otherGameHistory} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
