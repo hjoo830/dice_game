1466dab4bab39ea1c09cca270c7883e7a331c365
hjoo830 <hjoo830@naver.com>
2023-08-06 01:08:47 +0900

    apply css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Board from "./Board";
 import Button from './Button';
 import { useState } from 'react';
+import './App.css';
+import logo from './assets/logo.png';
 
 function random(n) {
     return Math.ceil(Math.random() * n);
@@ -24,12 +26,18 @@ function App() {
     };
 
     return (
-        <div>
-            <Button onClick={handleRollClick}>던지기</Button>
-            <Button onClick={handleClearClick}>처음부터</Button>
+        <div className="App">
             <div>
-                <Board name="나" color="blue" gameHistory={myHistory} />
-                <Board name="상대" color="red" gameHistory={otherHistory} />
+                <img className="App-logo" src={logo} alt={logo} />
+                <h1 className="App-title">주사위게임</h1>
+                <div>
+                    <Button className="App-button" color="blue" onClick={handleRollClick}>던지기</Button>
+                    <Button className="App-button" color="red" onClick={handleClearClick}>처음부터</Button>
+                </div>
+            </div>
+            <div className="App-boards">
+                <Board className="App-board" name="나" color="blue" gameHistory={myHistory} />
+                <Board className="App-board" name="상대" color="red" gameHistory={otherHistory} />
             </div>
         </div>
     );
